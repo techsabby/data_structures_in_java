8987cc841c8a2e0c2fb9fd7d3518d2638dde9a14
techsabby <sebastian.login@proton.me>
2023-08-25 06:41:02 -0400

    initial commit

---
+++ LinkedList.java
@@ -0,0 +1,24 @@
+class Node {
+    int number, position; 
+    Node next;
+
+    Node(int num, int pos) {
+        number = num;
+        pos = position;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    int size = 0; 
+    Node head = new Node();
+    Node tail = new Node();
+
+    public void addToHead() {
+        if (size == 0) {
+            size = 1;
+            head = tail = new Node(1, size);      
+        }
+    }
+}
+
