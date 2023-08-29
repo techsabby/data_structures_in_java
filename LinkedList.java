accab9d18b6612f6bc4032ea97ac0a71d511843e
techsabby <sebastian.login@proton.me>
2023-08-29 07:38:26 -0400

    Code improvements in readability

--- LinkedList.java
+++ LinkedList.java
@@ -1,114 +1,34 @@
 class Node {
     int number;
     Node next;
+
+    Node(int number) {
+        this.number = number;
+    }
 }
 
 public class LinkedList {
-    Node head;
-    Node tail;
+    Node head, tail;
 
-    /*
-        Methods:
+    /* 
+        Methods: 
 
         void printList();
-        void printHeadTail();
-        void addToHead(int number);
-        void addToTail(int number);
-        void deleteFromHead();
-        void deleteFromTail();
-
+        void addToHead();
     */
 
-    public void printList() {
-        Node tmp = head; 
-
-        if (head == null) {
-            System.out.println("Empty List.");
-        } else if (head != null && head.next == null) {
-            System.out.println("[" + head.number + "]");
-        } else {
-            while (tmp.next != null) {
-                System.out.print("[" + tmp.number + "]");
-                tmp = tmp.next;
-            }
-
-            System.out.println("[" + tmp.number + "]");
-        }
+    void printList() {
+        System.out.println(head.number);
     }
 
-    public void printHeadTail() {
-        if (head != null) {
-            System.out.println("Head contains: " + head.number);
-            System.out.println("Tail contains: " + tail.number);
-        } else {
-            System.out.println("Empty List.");
-        }
-    }    
-
-    public void addToHead(int number) {
-        Node node = new Node();
-        node.number = number; 
+    void addToHead(int number) {
+        Node node = new Node(number);
 
         if (head == null) {
             head = tail = node;
         } else {
-            node.next = head; 
+            node.next = head;
             head = node;
-        }
-    } 
-
-    public void addToTail(int number) {
-        Node node = new Node();
-        node.number = number;
-
-        if (head == null) {
-            head = tail = node;
-        } else {
-            Node tmp = head;
-
-            while(tmp.next != null) {
-                tmp = tmp.next;
-            }
-
-            tmp.next = node;
-            tail = node;
-        }
-    }
-
-    public void deleteFromHead() {
-        if (head == null) {
-            System.out.println("Empty List.");
-        } else if (head.next == null) { 
-            head = tail = null;
-        } else if (head.next.next == null) {
-            head = tail;
-        } else {
-            Node temp = new Node();
-
-            temp.next = head.next.next;
-            temp.number = head.next.number;
-            head = temp;
-        }
-    }
-
-    public void deleteFromTail() {
-        if (head == null) {
-            System.out.println("Empty List.");
-        } else if (head.next == null) {
-            head = tail = null;
-        } else if (head.next.next == null) {
-            tail = head;
-        } else {
-            Node temp = new Node();
-
-            temp = head;
-
-            while (temp.next.next != null) {
-                temp = temp.next;
-            }
-            
-            tail = temp;
-            tail.next = null;
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,25 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int decider, randonum;
-        LinkedList linkedlist = new LinkedList();
+        LinkedList linkedList = new LinkedList();
 
-        for (int i = 1; i <= 50; i++) {
-            decider = (int)Math.floor(Math.random() * (4 - 1 + 1) + 1);
-            randonum = (int)Math.floor(Math.random() * (100 - 1 + 1) + 1);
-            
-            if (decider == 1) 
-                linkedlist.addToHead(randonum);
-            else if (decider == 2) 
-                linkedlist.addToTail(randonum);
-            else if (decider == 3)
-                linkedlist.deleteFromHead();
-            else
-                linkedlist.deleteFromHead();
-
-            linkedlist.printList();
-        }
-
-        linkedlist.printHeadTail();
-        linkedlist.printList();
+        linkedList.addToHead(1);
+        linkedList.printList();
     }
 }
