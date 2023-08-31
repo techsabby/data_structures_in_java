fa1e6501561ac712b6f1e63359fc61f6f6465b81
techsabby <sebastian.login@proton.me>
2023-08-30 21:36:11 -0400

    Added sort and search functions to linkedlist

--- LinkedList.java
+++ LinkedList.java
@@ -2,23 +2,107 @@ class Node {
     int number;
     Node next;
 
+    Node() {}
+
     Node(int number) {
         this.number = number;
     }
 }
 
 public class LinkedList {
+    int size; 
+
     Node head, tail;
 
     /* 
         Methods: 
 
+        bool search(int);
         void printList();
-        void addToHead();
+        void printSize();
+        void lowHigh(int);
+        void highLow(int);
+        void addToHead(int);
+        void addToTail(int);
+        void deleteFromHead();
+        void deleteFromTail();
     */
 
+    
+    boolean search(int num) {
+        
+        int count = 0;
+        Node tmp = head;
+
+        while (tmp.next != null) {
+
+            if (tmp.number == num) 
+                count = count + 1;
+                
+            tmp = tmp.next;
+        }
+
+        if (count > 0) 
+            return true;
+        else    
+            return false;
+    }
+
     void printList() {
-        System.out.println(head.number);
+        Node tmp = head; 
+
+        if (head == null) {
+            System.out.println("Empty List.");
+        } else if (head != null && head.next == null) {
+            System.out.println("[" + head.number + "]");
+        } else {
+            while (tmp.next != null) {
+                System.out.print("[" + tmp.number + "]");
+                tmp = tmp.next;
+            }
+
+            System.out.println("[" + tmp.number + "]");
+        }
+    }
+
+    void printSize() {
+        System.out.println("Size: " + size);
+    }
+
+    void lowHigh(int size) {
+        int num;
+        int tmpSize = size;
+        Node tmp = head;
+
+        for(int i = 1; i < tmpSize; i++) {
+            if (tmp.number > tmp.next.number) {
+                num = tmp.number;
+                tmp.number = tmp.next.number;
+                tmp.next.number = num;
+
+                lowHigh(tmpSize - 1);
+            }
+
+            tmp = tmp.next;
+        }
+    }
+
+    void highLow(int size) {
+        int num;
+        int tmpSize = size;
+        Node tmp = head;
+
+        for(int i = 1; i < tmpSize; i++) {
+            if (tmp.number < tmp.next.number) {
+                num = tmp.number;
+                tmp.number = tmp.next.number;
+                tmp.next.number = num;
+
+                highLow(tmpSize -1);
+            }
+
+            tmp = tmp.next;
+        }
     }
 
     void addToHead(int number) {
@@ -30,5 +114,67 @@ public class LinkedList {
             node.next = head;
             head = node;
         }
+
+        size = size + 1;
+    }
+
+    void addToTail(int number) {
+        Node node = new Node(number);
+
+        if (head == null) {
+            head = tail = node;
+        } else {
+            Node tmp = head;
+
+            while(tmp.next != null) {
+                tmp = tmp.next;
+            }
+
+            tmp.next = node;
+            tail = node;
+        }
+
+        size = size + 1;
+    }
+
+    public void deleteFromHead() {
+        if (head == null) {
+            System.out.println("Empty List.");
+        } else if (head.next == null) { 
+            head = tail = null;
+        } else if (head.next.next == null) {
+            head = tail;
+        } else {
+            Node temp = new Node();
+
+            temp.next = head.next.next;
+            temp.number = head.next.number;
+            head = temp;
+        }
+
+        size = size - 1;
+    }
+
+    public void deleteFromTail() {
+        if (head == null) {
+            System.out.println("Empty List.");
+        } else if (head.next == null) {
+            head = tail = null;
+        } else if (head.next.next == null) {
+            tail = head;
+        } else {
+            Node temp = new Node();
+
+            temp = head;
+
+            while (temp.next.next != null) {
+                temp = temp.next;
+            }
+            
+            tail = temp;
+            tail.next = null;
+        }
+
+        size = size - 1;
     }
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
 
-        linkedList.addToHead(1);
-        linkedList.printList();
+        for (int i = 50; i >= 1; i--)
+            linkedList.addToTail(i);
+
+        System.out.println(linkedList.search(51));
+
+
     }
 }
